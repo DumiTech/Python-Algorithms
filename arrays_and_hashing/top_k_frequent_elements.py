e41c3939bd723def183d857e65fa793358e10a49
dumiTech <cernei_dumitru@protonmail.com>
2022-09-13 23:30:58 +0300

    top k frequent elements

--- arrays_and_hashing/top_k_frequent_elements.py
+++ arrays_and_hashing/top_k_frequent_elements.py
@@ -5,6 +5,10 @@ frequent elements. You may return the answer in any order.
 
 
 
+import heapq
+from typing import Counter
+
+
 class Solution:
     def top_k_frequent(nums, k):
         """
@@ -13,41 +17,46 @@ class Solution:
         :rtype: List[int]
         """
         ### Solution 1 ###
-        # d = {}
-        # response = []
+        d = {}
+        response = []
 
-        # for i in nums:
-        #     if i not in d:
-        #         d[i] = [i]
-        #     else:
-        #         d[i].append(i)
+        for i in nums:
+            if i not in d:
+                d[i] = [i]
+            else:
+                d[i].append(i)
 
-        # while k > 0:
+        while k > 0:
             # max -> the function that grabs the biggest value
             # d -> dictionary, it iterates through and grabs each key
             # key -> overrides the default behavior of the max function
             # lambda x -> defines a lambda to handle new behavior of max
             # d[x] -> the list defined by key 'x'
-        #     max_key = max(d, key = lambda x: len(list(d[x])))
-        #     response.append(max_key)
-        #     d.pop(max_key)
-        #     k -= 1
+            max_key = max(d, key = lambda x: len(list(d[x])))
+            response.append(max_key)
+            d.pop(max_key)
+            k -= 1
         
-        # return response
+        return response
 
 
         ### Solution 2 ###
-        d = {}
-        for i in nums:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
+        # d = {}
+        # for i in nums:
+        #     if i in d:
+        #         d[i] += 1
+        #     else:
+        #         d[i] = 1
 
-        arr = sorted(d.keys(), key = d.get, reverse=True)
+        # arr = sorted(d.keys(), key = d.get, reverse=True)
+        # print(arr[:k])
+        # return arr[:k]
 
-        return arr[:k]
 
+        ### Solution 3 ###
+        # count = Counter(nums)
+
+        # return heapq.nlargest(k, Counter(nums).keys(), key=Counter(nums).get)
 
 
     nums = [1,1,1,2,2,3]
